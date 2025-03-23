c7be6bcabd81943a5a04f64d12059c240aac7698
jojo lempk <100053811+pooppoopoooo@users.noreply.github.com>
2025-03-23 17:56:49 -0500

    Update ffconf.h

--- ffconf.h
+++ ffconf.h
@@ -2,13 +2,13 @@
 #define FF_FS_READONLY 0
 #define FF_FS_MINIMIZE 0
 #define FF_USE_LABEL 0
-#define FF_CODE_PAGE 932 //932
+#define FF_CODE_PAGE 437
 #define FF_USE_LFN 0
 #define FF_FS_RPATH 0
 #define FF_VOLUMES 1
 #define FF_MULTI_PARTITION 0
 #define FF_MAX_SS 512
-#define FF_FS_EXFAT 0
+#define FF_FS_EXFAT 1
 #define FF_NORTC_MON 1
 #define FF_NORTC_MDAY 1
 #define FF_NORTC_YEAR 2019
